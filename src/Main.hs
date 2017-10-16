1c7b0b22fd8f04fa8dc1a08d45a0d6d0e8b0c529
Sam Jennings <jennins@tcd.ie>
2017-10-16 19:31:49 +0100

    initial outputParser - not working

--- src/Main.hs
+++ src/Main.hs
@@ -19,51 +19,49 @@ main = do
   putStrLn ("server open on port " ++ (number !! 0))
   listen sock 2
   chan <- newChan
-  _ <- forkIO $ fix $ \loop -> do
+
+  --address memory leak
+  _ <- forkIO (fix ( \loop -> do
     (_, _) <- readChan chan
-    loop
+    loop))
 
   mainLoop sock chan 0
 
 type Msg = (Int, String)
 
 mainLoop :: Socket -> Chan Msg -> Int -> IO ()
-mainLoop sock chan msgNum = do
+mainLoop sock chan joinId = do
   conn <- accept sock
-  forkIO (runConn conn chan msgNum sock)
+  forkIO (runConn conn chan joinId sock)
 
-  mainLoop sock chan $! msgNum + 1
+  mainLoop sock chan $! joinId + 1
 
 
 runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> Socket -> IO ()
-runConn (sock, addr) chan msgNum parentSock = do
-    let broadcast msg = writeChan chan (msgNum, msg)
+runConn (sock, addr) chan joinId parentSock = do
+
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
 
-    hPutStrLn hdl "Welcome. Please send the following to join a chat room:"
-    hPutStrLn hdl "JOIN_CHATROOM: [chatroom name]\nCLIENT_IP: [IP Address of client if UDP | 0 if TCP]\nPORT: [port number of client if UDP | 0 if TCP]\nCLIENT_NAME: [string Handle to identifier client user]"
-    line <- fmap init (hGetLine hdl)
-    let Just room = stripPrefix "JOIN_CHATROOM: " line
-    line <- fmap init (hGetLine hdl)
-    let Just ip = stripPrefix "CLIENT_IP: " line
-    line <- fmap init (hGetLine hdl)
-    let Just port = stripPrefix "PORT: " line
-    line <- fmap init (hGetLine hdl)
-    let Just name = stripPrefix "CLIENT_NAME: " line
+    let rooms = "":rooms --hacky
+    hPutStrLn hdl "You are now connected to the server"
+    --hPutStrLn hdl "JOIN_CHATROOM: [chatroom name]\nCLIENT_IP: [IP Address of client if UDP | 0 if TCP]\nPORT: [port number of client if UDP | 0 if TCP]\nCLIENT_NAME: [string Handle to identifier client user]"
+    name <- fmap init (hGetLine hdl)
 
-    hPutStr hdl ("JOINED_CHATROOM: "++name++"\nCLIENT_IP: "++ip++"\nPORT: "++port++"\nROOM_REF: \nJOIN_ID: \n")
 
     commLine <- dupChan chan
 
     -- fork off a thread for reading from the duplicated channel
-    reader <- forkIO $ fix $ \loop -> do
-        (nextNum, line) <- readChan commLine
-        when (msgNum /= nextNum) $ hPutStrLn hdl line
-        loop
+    reader <- forkIO ( fix ( \loop -> do
+        (msgId, line) <- readChan commLine
+        if joinId /= msgId
+        then  forkIO(inputParser hdl line rooms) >> loop
+        else loop
+        ))
 
     handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
         line <- fmap init (hGetLine hdl)
+        let msg = line : msg
         case line of
              -- If an exception is caught, send a message and break the loop
              "quit" -> hPutStrLn hdl "Bye!"
@@ -72,11 +70,13 @@ runConn (sock, addr) chan msgNum parentSock = do
 
              "HELO text" -> heloText hdl addr >> loop
 
+             "send"      -> outputParser msg chan joinId >> loop
+
              -- else, continue looping.
-             _      -> broadcast (name ++ ": " ++ line) >> loop
+             _      -> loop
 
     killThread reader                      -- kill after the loop ends
-    broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
+    --broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
     hClose hdl                             -- close the handle
 
 heloText :: Handle -> SockAddr -> IO()
@@ -84,3 +84,31 @@ heloText hdl addr = do
   (Just hostName, Just serviceName) <- getNameInfo [] True True addr
   hPutStr hdl ("HELO text\nIP:"++hostName++"\nPort:"++serviceName++"\nStudentID:13326255\n")
 
+inputParser :: Handle -> String -> [String]-> IO()
+inputParser hdl line rooms= do
+  let msg = lines line
+  let Just room = stripPrefix "CHAT: " (msg !! 0)
+  if (True)---room `elem` rooms)
+  then hPutStrLn hdl (line)
+  else return()
+  --when (room `elem` rooms) --fails
+
+outputParser :: [String] -> Chan Msg -> Int -> IO()
+outputParser msg chan joinId = do
+  let broadcast msg = writeChan chan (joinId, msg)
+  let a = unlines msg
+  broadcast(a)
+
+  return ()
+
+--line <- fmap init (hGetLine hdl)
+--    let Just room = stripPrefix "JOIN_CHATROOM: " line
+--    let rooms = room:rooms
+--    line <- fmap init (hGetLine hdl)
+--    let Just ip = stripPrefix "CLIENT_IP: " line
+--    line <- fmap init (hGetLine hdl)
+--    let Just port = stripPrefix "PORT: " line
+--    line <- fmap init (hGetLine hdl)
+--    let Just name = stripPrefix "CLIENT_NAME: " line
+
+--    hPutStr hdl ("JOINED_CHATROOM: "++rooms !! 0++"\nCLIENT_IP: "++ip++"\nPORT: "++port++"\nROOM_REF: \nJOIN_ID: \n")
